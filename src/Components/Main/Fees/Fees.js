be927146fb102515f8787181b6114f40ad9c43a2
Liam Winfield <liambird3@gmail.com>
2023-10-20 16:33:10 +1100

    properly set up tailwind

--- src/Components/Main/Fees/Fees.js
+++ src/Components/Main/Fees/Fees.js
@@ -1,12 +1,58 @@
 import React from 'react';
-
+import './Fees.css'
 
 
 function Fees () {
     return (
         <div id='/Fees'>
-                
+            <div  className='Fees'>
+              <h1 className='Fees-h1'>Fees</h1>  
+              <p className='Fees-p'>Most clients are referred by their GP under a 
+                Mental Health Treatment Plan but can also be 
+                accessed through a specialist. All referrals 
+                are to be faxed, mailed or emailed  before an 
+                appt can be organised. Please contact Suzy to 
+                arrange any appointments and she will be able 
+                to assist. Contact details can be found on the 
+                appropriate page. 
+<br></br>
+
+A receipt will be given upon payment of upfront 
+cost and a rebate from Medicare will be organised 
+which can take up to 48 hours to be processed.
+<br></br>
+
+You are able to claim up to 10 sessions per calendar 
+year under a Mental Health Treatment Plan. The initial 
+referral covers you for 6 sessions then another appointment 
+is required with your GP to access the last 4.
+<br></br>
+</p>
+              <h2 className='Fees-h2'>Payment options</h2>
+              <p className='Fees-p'>The upfront costs can be paid via cash, EFT, 
+                cheque or direct deposit</p>
+              <h2 className='Fees-h2'>Upfront Fees Per Session</h2>
+              <p className='Fees-p'>$150 per session ($62.55 out of pocket after 
+                medicare rebate)<br></br>
+                 $130 per session for healthcare card holders 
+                 ($42.55 out of pocket after medicare rebate)
+</p>
+              <h2 className='Fees-h2'>ANY APPOINTMENT TIMES MADE AFTER 5PM WILL 
+                INCUR A $20 AFTER HOURS LEVY. </h2>
+
+                <p className='Fees-p'>If a Mental Health Treatment Plan is not organised, 
+                    full fees are to be paid although some clients are 
+                    able to claim a rebate through their private health 
+                    insurance. <br></br>
+
+                  Accredited options are Workcover, TAC,  DVA, NDIS 
+                  or VOCAT.</p>
+                  
+                <h2 className='Fees-h2'>Cancellations</h2>
+                <p className='Fees-p'> 24 hours notice is needed when cancelling 
+                appointments otherwise a fee will be incurred</p>
         </div>
+    </div>
 
     );
 }
