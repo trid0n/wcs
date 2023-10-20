ff4b3e927539124bd95a9806c4ab9ef68c17042e
Liam Winfield <liambird3@gmail.com>
2023-10-20 22:57:59 +1100

    rearranged aboutus

--- src/Components/Main/AboutUs/AboutUs.js
+++ src/Components/Main/AboutUs/AboutUs.js
@@ -1,49 +1,48 @@
 import React from 'react';
-import Dad from '../../Assets/Dad.jpg'
-import './AboutUs.css'
+import Dad from '../../Assets/Dad.jpg';
+import './AboutUs.css';
 
-function AboutUs () {
-    return (
-        <div className='AboutUsContainer' id='/About'> 
-            <div className='PicOfDavid'>
-            <img src={Dad} alt="A picture of David Winfield" />
-            </div>
-            <div className='AboutUsParagraph'>
-            <section>
-        <h2>Practitioner</h2>
-        <article>
-            <h3>Registered Psychologist</h3>
-            <p>David Winfield</p>
-        </article>
-    </section>
-
-    <section>
-        <h2>Qualifications</h2>
-        <p>BA Psychology & Sociology, Graduate Diploma Educational Psychology</p>
-    </section>
-
-    <section>
-        <h2>Professional Memberships</h2>
-        <p>AHPRA</p>
-    </section>
-
-    <section>
-        <h2>Psych Registration No</h2>
-        <p>PSY6001123668</p>
-    </section>
-
-    <section>
-        <h2>Experience</h2>
-        <p>
+function AboutUs() {
+  return (
+    <div className='AboutUsContainer' id='/About'>
+      <div className='PicOfDavid'>
+        <img src={Dad} alt="A picture of David Winfield" />
+      </div>
+      <h1 className='title'>Practitioner</h1>
+      <div className='AboutUsParagraph'>
+        <table className='centered-table'>
+          <tbody>
+            <tr>
+              <td>
+                <h3 className='table-title'>Registered Psychologist</h3>
+                <p>David Winfield</p>
+              </td>
+              <td>
+                <h3 className='table-title'>Qualifications</h3>
+                <p>BA Psychology & Sociology, Graduate Diploma Educational Psychology</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h3 className='table-title'>Professional Memberships</h3>
+                <p>AHPRA</p>
+              </td>
+              <td>
+                <h3 className='table-title'>Psych Registration No</h3>
+                <p>PSY6001123668</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <section>
+          <h3 className='Experience-title'>Experience</h3>
+          <p><i>
             I have worked for over 30 years in the field of Psychology both in the public and private sector. I worked for GARRS which supported people who were in recovery of a severe mental illness. For over 10 years I worked as a practitioner and Team Leader for Connections & Anglicare in the Families First program. I now work in private practice and enjoy bringing about change in people's lives.
-        </p>
+            </i></p>
         </section>
-            </div>
-              
-        </div>
-
-    );
+      </div>
+    </div>
+  );
 }
-    
 
 export default AboutUs;
